c7bb3024b99ae4edc0f43175ae4bd58faa8586bc
Jeremiah Rodden <jrodden1@gmail.com>
2019-08-16 17:03:25 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artist1 = Artist.new(name: "Andrew Peterson", bio: "Folk Singer/Songwriter, Fiction Author")
+artist2 = Artist.new(name: "Ralph Vaughn-Williams", bio: "Classical Composer")
+artist3 = Artist.new(name: "Matt Maher", bio: "Christian Music Artist")
+
+artist1.genres << Genre.create(name: "folk")
+artist2.genres << Genre.create(name: "classical")
+artist3.genres << Genre.create(name: "christian")
+
+artist1.songs << Song.create(name: "Dancing in the Minefields", genre_id: 1)
+artist2.songs << Song.create(name: "Fantasia on a Theme, Thomas Tallis", genre_id: 2)
+artist3.songs << Song.create(name: "Bread of Life", genre_id: 3)
+
+artist1.save 
+artist2.save 
+artist3.save 
+
+puts artist1
+puts artist2
+puts artist3
