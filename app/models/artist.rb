2874a925a0e4454021ba28b71d02af66df90b95e
George <georuiz817@gmail.com>
2019-08-31 11:30:45 -0400

    models complete

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,4 @@
 class Artist < ApplicationRecord
+    has_many :songs, dependent: :destory
+    has_many :genres, through: :songs
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,4 @@
 class Genre < ApplicationRecord
+    has_many :songs, dependent: :destory
+    has_many :artists, through: :songs
 end
